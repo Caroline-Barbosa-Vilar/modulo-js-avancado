f787b54e8afae7a9ae42f4f995e54ffb6c8cec82
Caroline Barbosa <bo.varig@gmail.com>
2022-07-25 20:58:54 -0300

    created the async await code exercise

---
+++ js-avancado-asyn-await/js/script.js
@@ -0,0 +1,45 @@
+let boilWater = (kettlesOnTheStove, stoveIsOn) => {
+  return new Promise((resolve, reject) => {
+
+    if(kettlesOnTheStove && stoveIsOn) {
+      console.log('Start boiling water');
+      resolve();
+    } else {
+      console.log('You must turn on the stove and put the kettle on');
+      reject();
+    }
+  });
+};
+
+let makingCoffee = (boiledWater) => {
+  return new Promise((resolve) => {
+    console.log('Coffee is ready!');
+    resolve(true);
+  })
+}
+
+let havingCoffee = (coffeeMade) => {
+  return new Promise((resolve) => {
+    console.log('Finish my coffee!');
+    resolve(true);
+  })
+}
+
+let washTheCup = (coffeeDrinked) => {
+  return new Promise((resolve) => {
+    console.log('Finish washing the cup');
+    resolve(true);
+  })
+}
+
+let kettlesOnTheStove = true;
+let stoveIsOn = true;
+
+async function starToMakingCoffee() {
+  const waterReady = await boilWater(kettlesOnTheStove, stoveIsOn);
+  const coffeeReady = await boiledWater(waterReady);
+  const coffeeFinished = await coffeeDrinked(coffeeReady);
+  const washedCup = await washTheCup(coffeeFinished);
+    if (washedCup) console.log('Coffee Ritual concludes');
+}
+starToMakingCoffee();

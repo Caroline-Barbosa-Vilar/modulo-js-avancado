37254af1b6f6ddfdc5c3b1f540efbaa78b1a6950
Caroline Barbosa <bo.varig@gmail.com>
2022-08-09 09:38:18 -0300

    export import exercise

---
+++ js-avancado-import-export/js/operations.js
@@ -0,0 +1,9 @@
+function sum(number1, number2) {
+  return number1 + number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+export { sum, divide}
---
+++ js-avancado-import-export/js/script.js
@@ -0,0 +1,3 @@
+import { sum, divide } from './operations.js';
+console.log(sum(10,10));
+console.log(divide(10,10));
